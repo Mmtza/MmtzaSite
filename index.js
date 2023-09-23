fccd6130f3d257e73d8522fd23dee2d8515e3016
Mmtzatzyy <114859674+Mmtzatzyy@users.noreply.github.com>
2023-09-24 03:44:17 +0700

    Menambahkan file js

---
+++ index.js
@@ -0,0 +1,15 @@
+var typingEffect = new Typed(".multiText", {
+  strings: ["fullstack web developer", "student", "gamer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+});
+
+function redirInstagram() {
+  window.location.href = "https://instagram.com/mmtza.mm";
+}
+
+function redirWhatsapp() {
+  window.location.href = "https://wa.me/+6285875282178";
+}
